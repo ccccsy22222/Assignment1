847977e0615a1dc792ed8d032bf25c67f49a7f6b
CCCSY <timdawson542@gmail.com>
2024-02-16 18:26:04 +0800

    data cleaning script

---
+++ clean.py
@@ -0,0 +1,33 @@
+import pandas as pd
+
+
+def clean_data(input1, input2, output):
+    
+    df1 = pd.read_csv(input1)
+    df2 = pd.read_csv(input2)
+    merged_df = pd.merge(df1, df2, left_on='respondent_id', right_on='id')
+
+
+    merged_df.drop('id', axis=1, inplace=True)
+
+
+    merged_df.dropna(inplace=True)
+
+
+    job_filter = ~merged_df['job'].str.contains('insurance|Insurance', case=False)
+    cleaned_df = merged_df[job_filter]
+
+
+    cleaned_df = cleaned_df[['respondent_id', 'name', 'address', 'phone', 'job', 'company', 'birthdate']]
+
+    cleaned_df.reset_index(drop=True, inplace=True)
+
+
+    cleaned_df.to_csv(output, index=False)
+if __name__ == "__main__":
+
+    input1 = r"/Users/cccsy/Downloads/AI/AIAssignment/respondent_contact.csv"
+    input2 = r"/Users/cccsy/Downloads/AI/AIAssignment/respondent_other.csv"
+    output = r"/Users/cccsy/Downloads/AI/AIAssignment/respondent_cleaned.csv"
+
+    clean_data(input1, input2, output)
